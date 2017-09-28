11be6cf9417f5833a09a1550eb5d48e53da10d54
Jai Goyal <goyaljai.y14@gmail.com>
2017-09-28 22:44:56 +0530

    Update binary_search_tree.h

--- forest/binary_search_tree.h
+++ forest/binary_search_tree.h
@@ -210,6 +210,53 @@ namespace forest {
                         }
                         return nullptr;
                 }
+                
+                
+                binary_search_tree_node <key_t,value_t> *minValueNode(binary_search_tree_node <key_t, value_t> *node)
+                {
+                    binary_search_tree_node <key_t, value_t> *current = node;
+
+                    while (current->left != nullptr)
+                        current = current->left;
+
+                    return current;
+                }
+
+                const binary_search_tree_node <key_t,value_t> *delete(key_t key)
+                {
+                        if (root == nullptr) return root;
+
+                        if(key < root->key)
+                        {
+                                root->left = delete(root->left,key);
+                        }
+                        else
+                        {
+                                root->right = delete(root->right,key);
+                        }
+                        else
+                        {
+                                if (root->left == nullptr)
+                        {
+                            binary_search_tree_node <key_t, value_t> *temp = root->right;
+                            return temp;
+                        }
+                        else if (root->right == nullptr)
+                        {
+                            binary_search_tree_node <key_t, value_t> *temp = root->left;
+                            return temp;
+                        }
+                        // inorder
+                        binary_search_tree_node <key_t, value_t> *temp = minValueNode(root->right);
+                        root->key = temp->key;
+
+                        root->right = deleteNode(root->right, temp->key);
+
+                        }return root;
+
+                }
+                
+                
                 /**
                  * @brief Finds the node with the minimum key
                  * @return The node with the minimum key
